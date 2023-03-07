5e92d343904212eecc6ccbc4e825eaf1948c4da0
solvd cpiskunou <cpiskunou.laba@solvd.com>
2023-03-07 12:00:10 +0300

    init

---
+++ src/main/java/by/piskunou/solvdlaba/MicroAirportLocationApplication.java
@@ -0,0 +1,13 @@
+package by.piskunou.solvdlaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAirportLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAirportLocationApplication.class, args);
+	}
+
+}
